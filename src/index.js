39b19a52c9a44e2bc1db5564cbc2f141d25737bd
Adrian Lacheta <46226061+adiczq@users.noreply.github.com>
2023-03-30 18:35:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 const search = document.querySelector(".search");
 const li = document.querySelectorAll("li");
 
-const searchin = (e) => {
-  const text = e.target.value.toLowerCase();
-  console.log(text);
+// const searchin = (e) => {
+//   const text = e.target.value.toLowerCase();
+//   console.log(text);
 
+//   li.forEach((el) => {
+//     const task = el.textContent.toLowerCase();
+
+//     if (task.indexOf(text) !== -1) {
+//       el.style.display = "block";
+//     } else {
+//       el.style.display = "none";
+//     }
+//   });
+// };
+
+const searchin = () => {
   li.forEach((el) => {
-    const task = el.textContent.toLowerCase();
-
-    if (task.indexOf(text) !== -1) {
-      el.style.display = "block";
-    } else {
+    const match = new RegExp(search.value, "i").test(el.textContent);
+    if (!match) {
       el.style.display = "none";
+    } else {
+      el.style.display = "block";
     }
   });
 };
