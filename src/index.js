5f4d1fbed71db8b46753ecf091916e56d8adfcc3
Adrian Lacheta <46226061+adiczq@users.noreply.github.com>
2023-03-30 18:24:54 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const search = document.querySelector(".search");
+const li = document.querySelectorAll("li");
+
+const searchin = (e) => {
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+
+  li.forEach((el) => {
+    const task = el.textContent.toLowerCase();
+
+    if (task.indexOf(text) !== -1) {
+      el.style.display = "block";
+    } else {
+      el.style.display = "none";
+    }
+  });
+};
+
+search.addEventListener("keyup", searchin);
